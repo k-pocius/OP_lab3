26eb0e60f1b426d9d0fc8aa965c6071097553c00
k-pocius <pockristijonas@gmail.com>
2025-05-21 15:54:01 +0300

    .

---
+++ lib.h
@@ -0,0 +1,42 @@
+#ifndef LIB_H
+#define LIB_H
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <chrono>
+#include <ctime>
+#include <fstream>
+#include <sstream>
+#include <time.h>
+#include <random>
+#include <list>
+#include <deque>
+#include <algorithm>
+#include <utility>
+
+using std::string;
+//using std::vector;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::ifstream;
+using std::istringstream;
+using std::ofstream;
+using std::setw;
+using std::left;
+using std::fixed;
+using std::setprecision;
+using std::sort;
+using std::swap;
+using std::numeric_limits;
+using std::streamsize;
+using std::ostringstream;
+using std::list;
+using std::deque;
+using std::copy;
+using std::ostream;
+using std::istream;
+using std::to_string;
+
+
+#endif
